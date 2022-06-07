2f316f2a3671599529387e9b6b41c11cbfb6d4ce
iCE CUBE <78229074+iDea-iCE-CUBE@users.noreply.github.com>
2022-06-07 11:04:57 +0800

    Add files via upload

---
+++ oldWeb/messageBoard.js
@@ -0,0 +1,4 @@
+var iId = new Array();
+var iContent = new Array();
+
+iId.unshift('iCE CUBE');iContent.unshift('PHP留言测试');iId.unshift('iCE CUBE');iContent.unshift('Nice');iId.unshift('iCE CUBE');iContent.unshift('在下面的文本框输入，然后点击右侧的红色按钮，即可发送');iId.unshift('iCE CUBE');iContent.unshift('WOWWWWWW');iId.unshift('Rad-C');iContent.unshift('加油哦！！！');iId.unshift('Rad-C');iContent.unshift('支持热爱者！');iId.unshift('iCE CUBE');iContent.unshift('谢谢');iId.unshift('');iContent.unshift('');iId.unshift('政委');iContent.unshift('HELLO WORLD');iId.unshift('政委');iContent.unshift('HELLO WORLD');iId.unshift('iCE CUBE');iContent.unshift('Test');iId.unshift('test');iContent.unshift('test');iId.unshift('iCE CUBE');iContent.unshift('TEST');iId.unshift('');iContent.unshift('傻逼');
