2429a88e6800414130b7b94b9b798433cc6d4d40
iCE CUBE <78229074+iDea-iCE-CUBE@users.noreply.github.com>
2022-11-23 16:37:31 +0800

    Add files via upload

---
+++ iceweb-github/oldWeb/messageBoard.js
@@ -0,0 +1,4 @@
+var iId = new Array();
+var iContent = new Array();
+
+iId.unshift('iCE CUBE');iContent.unshift('留言板功能砍掉了，因为没有动态服务器支持');iId.unshift('Rad-C');iContent.unshift('加油哦！！！');iId.unshift('Rad-C');iContent.unshift('支持热爱者！');iId.unshift('iCE CUBE');iContent.unshift('谢谢');iId.unshift('政委');iContent.unshift('HELLO WORLD');
