21db2364fb98207124a12c0990af0b38135f73ad
iCE CUBE <78229074+iDea-iCE-CUBE@users.noreply.github.com>
2023-12-28 14:03:23 +0800

    Update class13.js

--- other/class13/class13.js
+++ other/class13/class13.js
@@ -39,13 +39,13 @@
         }
 
         //今天没有课表
-/**
+
         let oMessage = document.getElementById("message");
             oMessage.style.fontSize ="50px";
             oMessage.style.position ="absolute";
             oMessage.style.left ="300px";
             oMessage.style.top ="300px";
-            oMessage.textContent = "今天不知道课表";
+            oMessage.textContent = "SUPPORTED BY NaOH";
             setTimeout(function () {
         
                 for (let i = 0; i < oLi.length; i++) {
@@ -60,4 +60,6 @@
                     oMessage.style.display ="none";
                 }, 1500);
             }, 2000);
-**/
+
+
+document.getElementsByTagName('html')[0].style.background = 'url(https://s11.ax1x.com/2023/12/28/piqWLXq.png) center/cover';
